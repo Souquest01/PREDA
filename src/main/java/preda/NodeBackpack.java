9498816ed9f7236786603478a842c00e20f523dc
Souquest01 <100442802+Souquest01@users.noreply.github.com>
2022-11-18 15:34:28 +0000

    18/11

--- src/main/java/preda/NodeBackpack.java
+++ src/main/java/preda/NodeBackpack.java
@@ -4,11 +4,19 @@
  */
 package preda;
 
+import java.text.DecimalFormat;
+
 /**
  * Node class, hold the information of the objects to be
  * stored in the backpack, weight as per the total value of the object,
  * value, as per the perceived value of the object and ratio, which is
  * the ratio weight/value to assign a priority while comparing objects.
+ * 
+ * 
+ * Clase nodo, almacena la información del objeto a ser añadido en la
+ * clase mochila, weight determina el peso total del objeto, value el
+ * valor percibido del objeto y ratio el ratio entre peso y valor, será
+ * la base para elegir la prioridad mientras se comparan objetos.
  * 
  */
 public class NodeBackpack {
@@ -37,14 +45,28 @@ public NodeBackpack(int weight, int value){
         return this.weight;
     }
     
-    public void change_percent(int percent){
+    public double get_value(){
+        return this.value;
+    }
+    public void change_percent(double percent){
         if(percent > 100 || percent < 0 ){
             System.out.println("ERROR: INPUT CHANGE_PERCENT() ");
         }else{
-            this.percent = percent;
+            this.percent = percent;            
+            this.value = ((this.value * percent)/100);
         }
         
     }
     
-}
+    @Override
+    public String toString(){
+        DecimalFormat df = new DecimalFormat("0.00");
+        String x ;
+        
+        x  = (df.format(this.weight) + " " + df.format(this.percent) + " " + df.format(this.value));
+        
+        return x ;
+    }
 
+}
+ 
